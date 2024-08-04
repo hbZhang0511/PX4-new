2068c1c155e6289eaf572febd8cc45af57cd6d8b
hbZhang <hbZhang0511@163.com>
2024-08-04 14:05:13 +0800

    第一次更改

---
+++ PX4-Autopilot/build/px4_sitl_default/src/lib/component_information/checksums.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdint.h>
+namespace component_information {
+static constexpr uint32_t component_general_crc = 3186318362;
+static constexpr const char *component_general_sha256 = "da25e82eaf31bcfd2057837d2e8e9d8f9bd2d4b3c6fca094c5e9d37c10872f78";
+static constexpr uint32_t all_events_crc = 2174174309;
+static constexpr const char *all_events_sha256 = "5d37a2bac6b8708e530d741cd0d8af9c1f1f71d9f21540994ea8300d0a813314";
+}
---
+++ PX4-Autopilot/build/px4_sitl_default/src/lib/version/build_git_version.h
@@ -0,0 +1,20 @@
+
+/* Auto Magically Generated file */
+/* Do not edit! */
+#pragma once
+
+#define PX4_GIT_VERSION_STR "1dacb4cdef2d7145754fc788fa8dc482eed74b40"
+#define PX4_GIT_VERSION_BINARY 0x1dacb4cdef2d7145
+#define PX4_GIT_TAG_STR "v1.14.3"
+#define PX4_GIT_BRANCH_NAME "stable-v1.14.3"
+
+#define PX4_GIT_OEM_VERSION_STR  ""
+
+#define PX4_GIT_TAG_OR_BRANCH_NAME "v1.14.3" // special variable: git tag, release or master branch
+
+#define MAVLINK_LIB_GIT_VERSION_STR  "18955a04c7c7467e00ea42b704addb4a9c12b53a"
+#define MAVLINK_LIB_GIT_VERSION_BINARY 0x18955a04c7c7467e
+
+#define NUTTX_GIT_VERSION_STR  "de41e7feaeffaec3ce65327e9569e8fdb553ca3d"
+#define NUTTX_GIT_VERSION_BINARY 0xde41e7feaeffaec3
+#define NUTTX_GIT_TAG_STR  "v11.0.0"
